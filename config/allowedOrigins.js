ce2079836bd97970fc30337b64140059591506a8
iykeslim <iykeslim01@gmail.com>
2022-11-29 14:28:14 +0100

    adding mongodb creating schemas ticket ids and connections

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://giberishssss',
-    'http://giberishsssssh'
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
 ]
 
 module.exports = allowedOrigins
